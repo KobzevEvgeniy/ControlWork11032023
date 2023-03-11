29242293a63bf327d3138e65d09339eb60cecaaf
Evgeniy <public-judge@yandex.ru>
2023-03-11 21:37:43 +0300

    написал метод для формирования базового массива BaseArray

--- control_work/Program.cs
+++ control_work/Program.cs
@@ -1,3 +1,18 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите слова через пробел: ");
 string el = Console.ReadLine();
+
+
+
+
+string[] BaseArray(string stringArray)
+{
+    string[] word = stringArray.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string[] result = new string[word.Length];
+
+    for (int i = 0; i < word.Length; i++)
+    {
+        result[i] = Convert.ToString(word[i]);
+    }
+    return result;
+}
